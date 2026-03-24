3695c0230d8a3ed0b448a802a66f588d0919606c
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-03-24 03:32:58 +0200

    Create sec.cpp

---
+++ LECTURE-CODING/lec_2/sec.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+class Owner;
+class Car;
+class Engine{
+    private:
+    int cc;
+    int hp;
+    public:
+    void setEngin(int c,int h);
+    void displayEngine();
+                  };
+
+
+class Car {
+private:
+    string model;
+    string makeDate;
+    Engine e;
+    int km;
+public:
+    void setEngine(int c,int h)
+    {
+        e.setEngin(c,h);
+
+    }
+    void setCar(const string &model,string makeDate,int km){
+    this->model=model;
+    this->makeDate=makeDate;
+    this->km=km;
+    }
+    const void showCar(){
+    cout<<"******"<<endl;
+    cout<<"model : "<<model<<endl;;
+    cout<<"makeDate : "<<makeDate<<endl;
+    cout<<"km :"<<km<<endl;
+    e.displayEngine();
+
+    }
+};
+class Owner{
+private:
+    string license;
+    string name;
+public:
+    void setOwner(string l, string n)
+    {
+        license=l;
+        name=n;
+    }
+        void showOwner(){
+    cout<<"******"<<endl;
+    cout<<"license : "<<license<<endl;;
+    cout<<"name : "<<name<<endl;
+    }
+    void driveCar(Car c)
+    {
+        cout<<"Owner in driving his car "<<license<<endl;;
+        c.showCar();
+    }
+
+};
+
+    void Engine::setEngin(int c,int h)
+    {
+        cc=c;hp=h;
+    }
+    void Engine::displayEngine()
+    {
+    cout<<"******"<<endl;
+    cout<<"CC : "<<cc<<endl;;
+    cout<<"HP : "<<hp<<endl;
+    }
+
+
+
+int main()
+{
+    Owner basouny;
+    Car nissan;
+    Car mecedes;
+    basouny.setOwner("123456","Basouny Basouny");
+    nissan.setCar("Sunny","2023",12000);
+    mecedes.setCar("Mecedes","2005",13254);
+    nissan.setEngine(1400,200);
+    basouny.driveCar(nissan);
+    nissan.showCar();
+
+
+
+    return 0;
+}
